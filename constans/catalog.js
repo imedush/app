9ef90f0fdad4bdf9a9f83e3d863b41d1eaf424f3
imedush <imedush@gmail.com>
2023-04-24 20:07:28 +0300

    Changes to be committed: 	new file:   Utils/LocalStorageUtil.js 	new file:   components/Header/Header.css 	new file:   components/Header/Header.js 	new file:   components/Products/Products.css 	new file:   components/Products/Products.js 	new file:   components/Shoping/Shoping.css 	new file:   components/Shoping/Shoping.js 	new file:   constans/catalog.js 	new file:   constans/root.js 	new file:   css/index.css 	new file:   img/catalog/EPIPHONE.jpg 	new file:   img/catalog/FENDER.jpg 	new file:   img/catalog/GRETSCH.jpg 	new file:   img/catalog/GRG140.jpg 	new file:   img/catalog/HONEY.jpg 	new file:   img/catalog/IBANEZ.jpg 	new file:   img/catalog/SQUIER.jpg 	new file:   img/catalog/STANDARD.jpg 	new file:   img/catalog/Soulmaster.jpg 	new file:   img/catalog/VINTAGE.jpg 	modified:   index.html

---
+++ constans/catalog.js
@@ -0,0 +1,62 @@
+const CATALOG = [
+    {
+        id: 'el1',
+        name: 'EPIPHONE LES PAUL CLASSIC WORN EBONY',
+        img: 'img/catalog/EPIPHONE.jpg',
+        price: 13248,
+    },
+    {
+        id: 'el2',
+        name: 'SQUIER by FENDER AFFINITY TELE',
+        img: 'img/catalog/SQUIER.jpg',
+        price: 8550,
+    },
+    {
+        id: 'el3',
+        name: 'IBANEZ GRG270B',
+        img: 'img/catalog/IBANEZ.jpg',
+        price: 10975,
+    },
+    {
+        id: 'el4',
+        name: 'VGS Soulmaster VSM-120/7',
+        img: 'img/catalog/Soulmaster.jpg',
+        price: 22031,
+    },
+    {
+        id: 'el5',
+        name: 'FENDER AFFINITY TELE RW ',
+        img: 'img/catalog/FENDER.jpg',
+        price: 7950,
+    },
+    {
+        id: 'el6',
+        name: 'GRG140 SB',
+        img: 'img/catalog/GRG140.jpg',
+        price: 7000,
+    },
+    {
+        id: 'el7',
+        name: 'GRETSCH G2627T STREAMLINER',
+        img: 'img/catalog/GRETSCH.jpg',
+        price: 18312,
+    },
+    {
+        id: 'el8',
+        name: 'LES PAUL CLASSIC HONEY',
+        img: 'img/catalog/HONEY.jpg',
+        price: 14832,
+    },
+    {
+        id: 'el9',
+        name: 'PAUL SPECIAL VE VINTAGE',
+        img: 'img/catalog/VINTAGE.jpg',
+        price: 5760,
+    },
+    {
+        id: 'el10',
+        name: 'EPIPHONE LES PAUL STANDARD',
+        img: 'img/catalog/STANDARD.jpg',
+        price: 17712,
+    },
+];
