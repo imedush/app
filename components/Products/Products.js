fae52f6cb23f0933592c15519fc1b63faeb8389d
imedush <imedush@gmail.com>
2023-04-24 22:56:08 +0300

    modified:   Utils/LocalStorageUtil.js 	modified:   components/Products/Products.css 	modified:   components/Products/Products.js 	modified:   css/index.css

--- components/Products/Products.js
+++ components/Products/Products.js
@@ -9,7 +9,7 @@ class Products {
                 <li class="products-element">
                     <span class="products-element__name"> ${name}</span>
                     <img class="products-element__img" src="${img}" />
-                    <span class="products-element__prise"> ${price}</span>
+                    <span class="products-element__prise">💲 ${price.toLocaleString()} USD </span>
                     <button class="products-element__btn">Добавить в корзину</button>
 
                 </li>
